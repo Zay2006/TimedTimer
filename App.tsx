1d02bbfe32772e15332c80164cc4221f21738e5c
Zay2006 <iwrig0048@launchpadphilly.org>
2025-02-13 15:19:02 +0000

    W O W Z A

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import TimerDisplay from "./src/components/timer/TimerDisplay";
 import TimerControls from "./src/components/timer/TimerControls";
+import React from "react";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <div className="p-6 bg-white shadow-lg rounded-lg">
-        <TimerDisplay />
-        <TimerControls />
+        <TimerDisplay time={0} />
+        <TimerControls onStart={function (): void {
+          throw new Error("Function not implemented.");
+        } } />
       </div>
     </div>
   );
